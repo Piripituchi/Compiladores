bca5d9c9a2026e04e68febda43a3a0391931b7ed
Jesus Eduardo Angeles Hernandez <55214042+Piripituchi@users.noreply.github.com>
2021-09-23 02:17:06 -0500

    modificacion a metodo iterativo de afnd contruccion de subconjuntos no admitida

--- Practica1/subconjuntosafnd.py
+++ Practica1/subconjuntosafnd.py
@@ -2,7 +2,34 @@
 
 import re
 import sys
+from itertools import chain,combinations
 
+def conjunto_potencia(Estados):
+    return list(chain.from_iterable(combinations(Estados,r) for r in range(len(Estados)+1)))
+
+def finales_prima(Ep,Finales):
+    Fp=[]
+    for estado in Ep:
+        for e in estado:
+            if e in Finales:
+                Fp.append(estado)
+                break
+    return Fp
+
+def delta_prima(Ep,Sigma,delta):
+    dp={}
+    for estado in Ep:
+        for s in Sigma:
+            estado_sig=[]
+            for e in estado:
+                if (e,s) not in delta.keys():
+                    continue
+                for e_sig in delta[(e,s)]:
+                    if e_sig not in estado_sig:
+                        estado_sig.append(e_sig)
+            estado_sig.sort()
+            dp.setdefault((estado,s),tuple(estado_sig))
+    return dp
 
 class Automata:
 
@@ -12,43 +39,32 @@ class Automata:
         self.inicial=inicial
         self.Finales=Finales
         self.delta=delta
-
-    def completarAutomata(self):
-        for s in self.Sigma:
-            for q in self.Estados:
-                if (q,s) not in self.delta.keys():
-                    self.delta.setdefault((q,s),['\u03F4'])
-                    self.delta.setdefault(('\u03F4',s),['\u03F4'])
-        return
+        self.Ep=conjunto_potencia(self.Estados)
+        self.Fp=finales_prima(self.Ep,self.Finales)
+        self.dp=delta_prima(self.Ep,self.Sigma,self.delta)
 
 
     def transicion(self,estado,caracter):
         if caracter not in self.Sigma:
             return estado,False
-        # if self.delta[(estado,caracter)]=='\u03F4':
-        #     return '\u03F4',False
-        estados_sig=self.delta[(estado,caracter)]
-        #print("  Transicion (", estado,",",caracter,")  ->  ", estados_sig)
-        return estados_sig,True
+        if (estado,caracter) not in self.dp.keys():
+            return '\u03F4',False
+        estado_sig=self.dp[(estado,caracter)]
+        print("  Transicion (", estado,",",caracter,")  ->", estado_sig)
+        return estado_sig,True
 
     def validarCadena(self,cadena):
-        estado_act=list(self.inicial)
+        estado=self.inicial
         errores=[]
-        temp=[]
-        FLAG=True
-        for c in cadena:
-            temp=[]
-            for q in estado_act:
-                if q=='\u03F4':
-                    continue
-                estado_sig,FLAG=self.transicion(q,c)
-                if not FLAG:
-                    errores.append(c)
-                temp.extend(estado_sig)
-            estado_act=temp  
-        if len((set(estado_act) & set(self.Finales))) > 0:
+        for c in range(0,len(cadena)):
+            estado,FLAG=self.transicion(estado,cadena[c])
+            if estado=='\u03F4' and not FLAG:
+                break
+            if not FLAG:
+                errores.append(cadena[c])
+        if estado in self.Fp:
             if errores:
-                return str(cadena+" si esta en el lenguaje, se aplico manejo de error para "+','.join(set(errores)))
+                return str(cadena+" si esta en el lenguaje, se aplico manejo de error para "+','.join(errores))
             return str(cadena+" si esta en el lenguaje")
         else:
             return str("Cadena: "+cadena+" no es valida")
@@ -57,7 +73,7 @@ class Automata:
 def leerAfd(archivo):
     estados=list(''.join(re.split(',|\n',archivo.readline())))
     sigma=list(''.join(re.split(',|\n',archivo.readline())))
-    inicial=''.join(archivo.readline().splitlines())
+    inicial=(''.join(archivo.readline().splitlines()),)
     finales=list(''.join(re.split(',|\n',archivo.readline())))
     transiciones=re.split(',|\n',archivo.read())
     dic_transiciones={}
@@ -75,7 +91,6 @@ if __name__ == "__main__":
         Estados,Sigma,Inicial,Finales,delta=leerAfd(f)
         f.close()
         automata=Automata(Estados,Sigma,Inicial,Finales,delta)
-        automata.completarAutomata()
         try:
             while True:
                 cadena=input('Ingrese cadena a evaluar por el automata: ')
@@ -84,10 +99,6 @@ if __name__ == "__main__":
                 print("\n Presione ^C para terminar")
         except:
             print("\nFin del programa")
-        # cadena=input('Ingrese cadena a evaluar por el automata: ')
-        # valido=automata.validarCadena(cadena)
-        # print(valido)
-        # print("\n Presione ^C para terminar")
     else:
         print("Error: Entrada incorrecta ")
-        print("Ejemplo de entrada correcta: $ python prac1.py prueba.txt")
+        print("Ejemplo: $ python prac1.py prueba.txt")
